6215bbffe566203ac7c4fb581e3f23570736d0cd
Daniel Reinert <danielreinert185@gmail.com>
2021-10-27 20:06:29 +0200

    Add paper function

--- functions.js
+++ functions.js
@@ -1,9 +1,3 @@
-function printMessage(msg){
-	let div = document.createElement('div');
-	div.innerHTML = msg;
-	document.getElementById('messages').appendChild(div);
-}
-
-function clearMessages(){
-	document.getElementById('messages').innerHTML = '';
+let computerMove = `papier`;
+	printMessage('Zagrałem' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
 }
