c51c37255397a791873b02e40bf35333888165ea
Daniel Reinert <danielreinert185@gmail.com>
2021-11-03 20:18:19 +0100

    change paper function

--- js/script.js
+++ js/script.js
@@ -1,35 +1,4 @@
-let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+let computerMove = 'kamień';
+let playerMove = 'papier';
 
-console.log('Gracz wpisał: ' + playerInput);
-
-let playerMove = 'nieznany ruch';
-
-if(playerInput == '1'){
-  playerMove = 'kamień';
-}
-
-if(playerInput == '2'){
-    playerMove = 'papier';
-  }
-
-  if(playerInput == '3'){
-    playerMove = 'nożyce';
-  }
-
-printMessage('Twój ruch to: ' + computerMove);
-
-let computerMove = 'nieznany ruch';
-
-if(computerInput == '1'){
-    computerMove = 'kamień';
-}
-
-if(playerInput == '2'){
-    computerMove = 'papier';
-  }
-
-  if(playerInput == '3'){
-    computerMove = 'nożyce';
-  }
-
-printMessage('Twój ruch to: ' + computerMove);
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to' + playerMove + ',to wygrywasz!');
