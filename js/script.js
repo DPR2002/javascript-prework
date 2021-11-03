11cdaf7fa16025332a6d16f5b7000c482db6c161
Daniel Reinert <danielreinert185@gmail.com>
2021-11-03 20:43:45 +0100

    add logic game structure

--- js/script.js
+++ js/script.js
@@ -1,4 +1,59 @@
-let computerMove = 'kamień';
-let playerMove = 'papier';
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to' + playerMove + ',to wygrywasz!');
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if (playerInput == '1'){
+  playerMove = 'kamień';
+}
+
+else if (playerInput == '2'){
+    playerMove = 'papier';
+  }
+
+ else if (playerInput == '3'){
+    playerMove = 'nożyce';
+  }
+
+
+  let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if (randomNumber == 1){
+  computerMove = 'kamień';
+}
+
+else if (randomNumber == 2){
+    computerMove = 'papier';
+  }
+
+else if (randomNumber == 3){
+    computerMove = 'kamień';
+  }
+
+printMessage('Mój ruch to: ' + computerMove);
+
+printMessage('Twój ruch to: ' + playerMove);
+
+
+
+
+if( computerMove == 'kamień' && playerMove == 'papier'){
+    printMessage('Ty wygrywasz!');
+  }
+
+
+
+  else if(computerMove == playerMove){
+    printMessage('Jest remis!');
+  
+
+
+} else {
+printMessage ('Przegrałeś!');
+}
+  
