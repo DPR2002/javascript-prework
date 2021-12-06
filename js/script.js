b2d0b3ee188a8cf9a80bb708bbd7a180f774c3fe
Daniel Reinert <danielreinert185@gmail.com>
2021-12-06 22:00:39 +0100

    Add displayresult function

--- js/script.js
+++ js/script.js
@@ -13,54 +13,28 @@ function getMoveName(argMoveId){
   return 'nieznany ruch';
 }
 
-let computerMove = getMoveName(randomNumber);
-let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-
-
-if (playerInput == '1')
-  playerMove = 'kamień';
-}
-else if (playerInput == '2') {
-  playerMove = 'papier';
-}
-else if (playerInput == '3') {
-  playerMove = 'nożyce';
-}
-
-let randomNumber = Math.floor(Math.random() * 3 + 1);
-let computerMove = getMoveName(randomNumber);
-
-console.log('Wylosowana liczba: ' + randomNumber);
-console.log('Gracz wylosował: ' + playerInput);
-
-if (randomNumber == '1') {
-  computerMove = 'kamień';
-} else if (randomNumber == '2') {
-  computerMove = 'papier';
-} else {
-  computerMove = 'kamień'
-}
-
-console.log('moves:', argComputerMove, argPlayerMove);
 function displayResult(argComputerMove, argPlayerMove){
   printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
-if (
-  argComputerMove  =='kamień' && argPlayerMove =='papier' ||
-  argComputerMove  =='papier' && argPlayerMove =='nożyce' ||
-  argComputerMove  =='nożyce' && argPlayerMove =='kamień'
-) {
-  printMessage('Wygrałeś!');
-} else if (argComputerMove == argPlayerMove) {
-  printMessage('Remis!');
-} else {
-  printMessage('Przegrałeś!')
+  if (
+    argComputerMove  =='kamień' && argPlayerMove =='papier' ||
+    argComputerMove  =='papier' && argPlayerMove =='nożyce' ||
+    argComputerMove  =='nożyce' && argPlayerMove =='kamień'
+  ) {
+    printMessage('Wygrałeś!');
+  } else if (argComputerMove == argPlayerMove) {
+    printMessage('Remis!');
+  } else {
+    printMessage('Przegrałeś!')
+  }
 }
 
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+let randomNumber = Math.floor(Math.random() * 3 + 1);
 
-
-
-
-
+let computerMove = getMoveName(randomNumber);
+let playerMove = getMoveName(playerInput);
 
 printMessage('Mój ruch:' + computerMove);
 printMessage('Twój ruch:' + playerMove);
+
+displayResult(computerMove, playerMove);
